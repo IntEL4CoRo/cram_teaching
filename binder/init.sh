dfc598c74134e371803dc64bde8b35ae52a0b2ef
Yanxiang Zhan <yanxiang@uni-bremen.de>
2023-05-17 12:08:01 +0200

    Add binder support

---
+++ binder/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source ${HOME}/workspace/ros/devel/setup.bash
+
+roscore &
+roslaunch --wait rvizweb rvizweb.launch config_name:=cram_projection_demos &
+roslaunch --wait ${PWD}/binder/reset_pr2.launch msg_file:=${PWD}/binder/reset_pr2.txt &
+
+xvfb-run exec "$@"
