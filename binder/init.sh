3a9c5828ead35f076be8a5d3cddb674d8f4e9597
Yanxiang Zhan <yanxiang@uni-bremen.de>
2023-06-06 20:02:03 +0200

    update rviz config

--- binder/init.sh
+++ binder/init.sh
@@ -3,7 +3,7 @@
 source ${HOME}/workspace/ros/devel/setup.bash
 
 roscore &
-roslaunch --wait rvizweb rvizweb.launch &
+roslaunch --wait rvizweb rvizweb.launch config_file:=${HOME}/repo/binder/rviz_config.json &
 
 xvfb-run exec "$@"
 # exec "$@"
