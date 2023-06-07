ffe18ae7847bfccbe0616f63e45220a37052f2f3
Yanxiang Zhan <yanxiang@uni-bremen.de>
2023-06-07 12:21:26 +0200

    update rvizweb config

--- binder/update_scene.sh
+++ binder/update_scene.sh
@@ -2,6 +2,24 @@
 
 source ${HOME}/workspace/ros/devel/setup.bash
 
+
+# Kill the environment rosnodes
+nodes=$(rosnode list)
+# Nodes to exclude from killing
+exclude=("rosbridge_websocket" "tf2_web_republisher" "roswww" "rosapi" "rosout")
+
+# Loop through the nodes and kill the ones not in the exclusion list
+for node in $nodes; do
+  # Extract the node name
+  node_name=${node##*/}
+
+  # Check if the node should be excluded
+  if [[ ! " ${exclude[@]} " =~ " ${node_name} " ]]; then
+    # Kill the node
+    rosnode kill $node_name
+  fi
+done
+
 REPO_CONFIG_PATH=${HOME}/repo/binder
 
 # roslaunch cram_projection_demos apartment_tiago.launch
@@ -11,8 +29,11 @@ REPO_CONFIG_PATH=${HOME}/repo/binder
 # roslaunch cram_projection_demos household_donbot.launch
 # # roslaunch cram_pr2_popcorn_demo sandbox.launch
 # roslaunch cram_projection_demos retail_donbot.launch
-roslaunch cram_projection_demos $@ &
-# roslaunch ${REPO_CONFIG_PATH}/init_robot_pose.launch msg_file:=${HOME}/repo/binder/$@.txt &
-roslaunch rvizweb update_config.launch config_file:=${REPO_CONFIG_PATH}/rviz_config/rviz_config.json
+# roslaunch cram_projection_demos retail_pr2.launch
 
+roslaunch --wait cram_projection_demos ${@} &
+sleep 1
+roslaunch --wait rvizweb update_config.launch config_file:=${REPO_CONFIG_PATH}/rviz_config/${@}.json
+
+roslaunch --wait ${REPO_CONFIG_PATH}/init_robot_pose.launch msg_file:=${HOME}/repo/binder/rviz_config/${@}.txt
 # exec "$@"
