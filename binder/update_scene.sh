6d90eb7eaaaf159dfb16754316767103511deaa3
Yanxiang Zhan <yanxiang@uni-bremen.de>
2023-08-15 14:55:46 +0200

    Update jupyter environment

--- binder/update_scene.sh
+++ binder/update_scene.sh
@@ -2,13 +2,10 @@
 
 source ${HOME}/workspace/ros/devel/setup.bash
 
-# Kill the environment rosnodes
+# Environment rosnodes to be killed
 nodes=$(rosnode list)
-# Nodes to exclude from killing
+# Nodes to be excluded from killing
 exclude=("rosbridge_websocket" "roswww" "rosapi" "rosout", "ros_board")
-# exclude=("rosbridge_websocket" "tf2_web_republisher" "roswww" "rosapi" "rosout")
-
-# Loop through the nodes and kill the ones not in the exclusion list
 for node in $nodes; do
   # Extract the node name
   node_name=${node##*/}
@@ -21,7 +18,7 @@ for node in $nodes; do
 done
 
 # Launch new rosnodes
-REPO_CONFIG_PATH=${HOME}/repo/binder
+REPO_CONFIG_PATH=${REPO_DIR}/binder
 roslaunch ${REPO_CONFIG_PATH}/update_scene.launch  \
           launch_file:=${@} \
           config_path:=${REPO_CONFIG_PATH} &
